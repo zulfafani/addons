9748a42e2dcfaa86837768bc61709573caafe5b1
abhirama <abhi.f@visi-intech.id>
2024-06-04 17:30:05 +0700

    update

--- integrasi_odoo_pos/controllers/master_main.py
+++ integrasi_odoo_pos/controllers/master_main.py
@@ -9,31 +9,36 @@ def main():
     key = get_config('key').encode()
     config = Config(file_path, key)
 
-    instance1 = config.get_instance('odoo_db1')
-    instance2 = config.get_instance('odoo_db2')
-    # sql_db_config = config.get_instance('sql_db')
+    instanceMC = config.get_instance('odoo_db_mc')
+    instanceSS1 = config.get_instance('odoo_db1')
+    # instanceSS2 = config.get_instance('odoo_db3')
+    # instanceSS3 = config.get_instance('odoo_db4')
 
-    decrypted_password1 = config.decrypt_password(instance1['password'])
-    decrypted_password2 = config.decrypt_password(instance2['password'])
-    # decrypted_sql_password = config.decrypt_password(sql_db_config['password'])
+    decrypted_password1 = config.decrypt_password(instanceMC['password'])
+    decrypted_password2 = config.decrypt_password(instanceSS1['password'])
+    # decrypted_password3 = config.decrypt_password(instanceSS2['password'])
+    # decrypted_password4 = config.decrypt_password(instanceSS3['password'])
 
-    odoo_client1 = OdooClient(instance1['url'], instance1['db'], instance1['username'], decrypted_password1)
-    odoo_client2 = OdooClient(instance2['url'], instance2['db'], instance2['username'], decrypted_password2)
+    odoo_mc_client = OdooClient(instanceMC['url'], instanceMC['db'], instanceMC['username'], decrypted_password1)
+    odoo_ss1_client = OdooClient(instanceSS1['url'], instanceSS1['db'], instanceSS1['username'], decrypted_password2)
+    # odoo_ss2_client = OdooClient(instanceSS2['url'], instanceSS2['db'], instanceSS1['username'], decrypted_password3)
+    # odoo_ss3_client = OdooClient(instanceSS3['url'], instanceSS3['db'], instanceSS1['username'], decrypted_password4)
 
-    integrator = DataIntegrator(odoo_client1, odoo_client2)
+    integrator = DataIntegrator(odoo_mc_client, [odoo_ss1_client])
     # integrator.delete_data()
+
+    # Master MC to Store Server
     # integrator.transfer_data('res.partner', ['id','name', 'street', 'street2', 'phone', 'mobile', 'email', 'website','title','customer_rank', 'supplier_rank', 'write_uid', 'create_date', 'write_date'], 'Master Customer')
-    integrator.transfer_data('product.category', ['id', 'name', 'parent_id', 'property_valuation', 'write_uid', 'create_date', 'write_date'], 'Master Item Group')  # 'property_account_income_categ_id', 'product_account_expense_categ_id', 'property_cost_method', not exist
+    # integrator.transfer_data('product.category', ['id', 'display_name', 'name', 'parent_id', 'property_valuation', 'write_uid', 'create_date', 'write_date'], 'Master Item Group')  # 'property_account_income_categ_id', 'product_account_expense_categ_id', 'property_cost_method', not exist
     integrator.transfer_data('product.template', ['id', 'name', 'sale_ok', 'purchase_ok', 'detailed_type', 'invoice_policy', 'uom_id', 'uom_po_id', 'list_price', 'standard_price', 'taxes_id', 'categ_id',  'default_code', 'write_uid', 'create_date', 'write_date'], 'Master Item')
     # integrator.transfer_data('res.users', ['id', 'login', 'write_uid', 'create_date', 'write_date'], 'Master Users')
     # integrator.transfer_data('stock.location', ['id', name, location_id, usage, company_id, scrap_location, return_location, replenish_location, 'write_uid', 'create_date', 'write_date'], 'Master Location')
     # integrator.transfer_data('product.pricelist', ['id', 'name', 'currency_id', 'write_uid', 'create_date', 'write_date'], Master Pricelist Header) #perlu menambah lines/ detail
     # integrator.transfer_data('product.pricelist.item', ['id', min_quantity, date_start, date_end, 'write_uid', 'create_date', 'write_date'], Master Pricelist Detail) #perlu menambah lines/ detail name, price not exist
 
-    # sql_db = SQLDatabase(sql_db_config['server'], sql_db_config['port'], sql_db_config['database'], sql_db_config['user'], decrypted_sql_password)
-    # query_result = sql_db.execute_query('SELECT * FROM your_table')
-    # print(query_result)
-
+    # Transaksi Store Server to Master Console
+    integrator.transfer_transaksi('account.move', ['id', 'name', 'partner_id', 'invoice_date', 'payment_reference', 'invoice_date_due', 'journal_id', 'state', 'write_uid', 'create_date', 'write_date'], 'Transaksi Invoice Header')
+    integrator.transfer_transaksi('account.move.line', ['id', 'product_id', 'name', 'account_id', 'quantity', 'product_uom_id'],  )
 
 if __name__ == '__main__':
     main()
--- integrasi_odoo_pos/controllers/ss_invoice.py
+++ integrasi_odoo_pos/controllers/ss_invoice.py
@@ -2,12 +2,12 @@ from odoo import models, fields, api, SUPERUSER_ID
 from odoo.exceptions import UserError
 
 
-class IntegrationSalesOrder(models.Model):
-    _inherit = 'sale.order'
+class IntegrationInvoice(models.Model):
+    _inherit = 'account.move'
 
     BATCH_SIZE = 100
 
-    def sales_order_integration(self):
-        data_sales_order = self.target_client.call_odoo('object', 'execute_kw', self.target_client.db, self.target_client.uid,
-                                                 self.target_client.password, 'sale.order' , 'search_read', [[]],
+    def invoice_integration(self):
+        data_invoice = self.target_client.call_odoo('object', 'execute_kw', self.target_client.db, self.target_client.uid,
+                                                 self.target_client.password, 'account.move' , 'search_read', [[]],
                                                  {'fields': fields})
